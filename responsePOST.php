e1defa762ce806006ee28aad4312badca3ff5260
dordorgum <i@dordorgum.com>
2015-03-26 10:55:19 +0800

    u

--- responsePOST.php
+++ responsePOST.php
@@ -10,6 +10,7 @@ class wechatCallbackapiTest
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 		echo "test success - ".$postStr;
 		echo "_POST -".$_POST["data"];
+		echo "_POST dump -".var_dump($_POST);
 		
       	//extract post data
       	/*
